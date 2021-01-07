ddd257013d700249e47e6f4ad7cab818fbb74de6
Agustin <agucarranza@github.com>
2021-01-06 23:08:43 -0300

    mips module started, building mem stage

--- mips.srcs/sim_1/new/MEM_WB.v
+++ mips.srcs/sim_1/new/MEM_WB.v
@@ -1,43 +1,49 @@
 `timescale 1ns / 1ps
 
 module MEM_WB (
-	input              clk        , // Clock
-	input              rst        , // Asynchronous reset active low
+	input              clk               , // Clock
+	input              rst               , // Asynchronous reset active low
 	// Registers - IN
-	input  wire [31:0] i_Address  ,
-	input  wire [31:0] i_Read_data,
+	input  wire [31:0] i_Address         ,
+	input  wire [31:0] i_Read_data       ,
+	input  wire [ 4:0] i_MuxRegDst_result,
 	// WB - Control - IN
-	input  wire        i_RegWrite ,
-	input  wire        i_MemtoReg ,
+	input  wire        i_RegWrite        ,
+	input  wire        i_MemtoReg        ,
 	// Registers - OUT
-	output wire [31:0] o_Address  ,
-	output wire [31:0] o_Read_data,
+	output wire [31:0] o_Address         ,
+	output wire [31:0] o_Read_data       ,
+	output wire [ 4:0] o_MuxRegDst_result,
 	// WB - Control - OUT
-	output wire        o_RegWrite ,
+	output wire        o_RegWrite        ,
 	output wire        o_MemtoReg
 );
 	// Registers
-	reg [31:0] Address  ;
-	reg [31:0] Read_data;
+	reg [31:0] Address         ;
+	reg [31:0] Read_data       ;
+	reg [ 4:0] MuxRegDst_result;
 	// Control
 	reg RegWrite;
 	reg MemtoReg;
 
 	always @(posedge clk) begin : proc_registers
 		if(rst) begin
-			Address   <= 32'b0;
-			Read_data <= 32'b0;
+			Address          <= 32'b0;
+			Read_data        <= 32'b0;
+			MuxRegDst_result <= 5'b0;
 		end else begin
-			Address   <= i_Address;
-			Read_data <= i_Read_data;
+			Address          <= i_Address;
+			Read_data        <= i_Read_data;
+			MuxRegDst_result <= i_MuxRegDst_result;
 
 			RegWrite <= i_RegWrite   ;
 			MemtoReg <= i_MemtoReg   ;
 		end
 	end
 	// Registers
-	assign o_Address   = Address  ;
-	assign o_Read_data = Read_data;
+	assign o_Address          = Address  ;
+	assign o_Read_data        = Read_data;
+	assign o_MuxRegDst_result = MuxRegDst_result;
 	// Control
 	assign o_RegWrite = RegWrite   ;
 	assign o_MemtoReg = MemtoReg   ;
--- mips.srcs/sim_1/new/MIPS.v
+++ mips.srcs/sim_1/new/MIPS.v
@@ -1,6 +1,7 @@
 `timescale 1ns / 1ps
 `include "Mux.v" `include "PC.v" `include "Add.v" `include "Instruction_memory.v" `include "IF_ID.v"
 `include "Registers.v" `include "Control.v" `include "ID_EX.v"
+`include "ALU_control.v" `include "ALU.v" `include "EX_MEM.v"
 
 module MIPS
 	(
@@ -8,9 +9,12 @@ module MIPS
 		input rst  // Asynchronous reset active low
 	);
 // Control
-wire       PCSrc      ;
-wire       RegWrite   ;
-wire       RegDst   ;
+wire       PCSrc   ;
+wire       RegWrite;
+wire       RegDst  ;
+wire       ALUSrc  ;
+wire [1:0] ALUOp   ;
+
 wire       ID_RegDst  ;
 wire [1:0] ID_ALUOp   ;
 wire       ID_ALUSrc  ;
@@ -38,7 +42,22 @@ wire [31:0] IFID_to_IDEX_PC_Address           ;
 wire [31:0] Registers_to_IDEX_ReadData1       ;
 wire [31:0] Registers_to_IDEX_ReadData2       ;
 // EX
-wire [31:0] IDEX_to_EXMEM_PCAddress;
+wire [31:0] IDEX_to_Add_PCAddress                           ;
+wire [31:0] IDEX_to_ALU_ReadData1                           ;
+wire [31:0] IDEX_to_MuxALUSrc_to_EXMEM_ReadData2            ;
+wire [ 4:0] IDEX_to_MuxRegDst_rt_0                          ;
+wire [ 4:0] IDEX_to_MuxRegDst_rd_1                          ;
+wire [31:0] IDEX_to_ALUControl_to_Add_to_MuxALUSrc_Immediate;
+wire [31:0] MuxALUSrc_to_ALU_Operand2                       ;
+wire        ALU_to_EXMEM_Zero                               ;
+wire [ 3:0] ALUControl_to_ALU_Operation                     ;
+wire [31:0] Add_to_EXMEM_AddResult                          ;
+wire [31:0] ALU_to_EXMEM_ALUResult                          ;
+wire [ 4:0] MuxRegDst_to_EXMEM_Result                       ;
+
+
+
+
 
 
 
@@ -102,75 +121,99 @@ Control i_Control (
 );
 
 ID_EX i_ID_EX (
-	.clk          (clk                                         ),
-	.rst          (rst                                         ),
-	.i_PC_Address (IFID_to_IDEX_PC_Address                     ),
-	.i_Read_data_1(Registers_to_IDEX_ReadData1                 ),
-	.i_Read_data_2(Registers_to_IDEX_ReadData2                 ),
-	.i_Immediate  ({ {16{Instruction[15]}}, Instruction[15:0] }), // Sign extension
-	.i_rt         (Instruction[20:16]                          ),
-	.i_rd         (Instruction[15:11]                          ),
-	.i_RegWrite   (ID_RegWrite                                 ),
-	.i_MemtoReg   (ID_MemtoReg                                 ),
-	.i_Branch     (ID_Branch                                   ),
-	.i_MemRead    (ID_MemRead                                  ),
-	.i_MemWrite   (ID_MemWrite                                 ),
-	.i_RegDst     (ID_RegDst                                   ),
-	.i_ALUOp      (ID_ALUOp                                    ),
-	.i_ALUSrc     (ID_ALUSrc                                   ),
-	.o_PC_Address (o_PC_Address                                ),
-	.o_Read_data_1(o_Read_data_1                               ),
-	.o_Read_data_2(o_Read_data_2                               ),
-	.o_Immediate  (o_Immediate                                 ),
-	.o_rt         (o_rt                                        ),
-	.o_rd         (o_rd                                        ),
-	.o_RegWrite   (o_RegWrite                                  ),
-	.o_MemtoReg   (o_MemtoReg                                  ),
-	.o_Branch     (o_Branch                                    ),
-	.o_MemRead    (o_MemRead                                   ),
-	.o_MemWrite   (o_MemWrite                                  ),
-	.o_RegDst     (o_RegDst                                    ),
-	.o_ALUOp      (o_ALUOp                                     ),
-	.o_ALUSrc     (o_ALUSrc                                    )
+	.clk          (clk                                             ),
+	.rst          (rst                                             ),
+	.i_PC_Address (IFID_to_IDEX_PC_Address                         ),
+	.i_Read_data_1(Registers_to_IDEX_ReadData1                     ),
+	.i_Read_data_2(Registers_to_IDEX_ReadData2                     ),
+	.i_Immediate  ({ {16{Instruction[15]}}, Instruction[15:0] }    ), // Sign extension
+	.i_rt         (Instruction[20:16]                              ),
+	.i_rd         (Instruction[15:11]                              ),
+	.i_RegWrite   (ID_RegWrite                                     ),
+	.i_MemtoReg   (ID_MemtoReg                                     ),
+	.i_Branch     (ID_Branch                                       ),
+	.i_MemRead    (ID_MemRead                                      ),
+	.i_MemWrite   (ID_MemWrite                                     ),
+	.i_RegDst     (ID_RegDst                                       ),
+	.i_ALUOp      (ID_ALUOp                                        ),
+	.i_ALUSrc     (ID_ALUSrc                                       ),
+	.o_PC_Address (IDEX_to_Add_PCAddress                           ),
+	.o_Read_data_1(IDEX_to_ALU_ReadData1                           ),
+	.o_Read_data_2(IDEX_to_MuxALUSrc_to_EXMEM_ReadData2            ),
+	.o_Immediate  (IDEX_to_ALUControl_to_Add_to_MuxALUSrc_Immediate),
+	.o_rt         (IDEX_to_MuxRegDst_rt_0                          ),
+	.o_rd         (IDEX_to_MuxRegDst_rd_1                          ),
+	.o_RegWrite   (EX_RegWrite                                     ),
+	.o_MemtoReg   (EX_MemtoReg                                     ),
+	.o_Branch     (EX_Branch                                       ),
+	.o_MemRead    (EX_MemRead                                      ),
+	.o_MemWrite   (EX_MemWrite                                     ),
+	.o_RegDst     (RegDst                                          ),
+	.o_ALUOp      (ALUOp                                           ),
+	.o_ALUSrc     (ALUSrc                                          )
 );
 
 // EX
 
 Add i_Add (
-	.i_Operand_0 (Id ),
-	.i_Operand_1 (i_Operand_1 ),
-	.o_Add_result(o_Add_result)
+	.i_Operand_0 (IDEX_to_Add_PCAddress                                ),
+	.i_Operand_1 ((IDEX_to_ALUControl_to_Add_to_MuxALUSrc_Immediate<<2)), // Shift left 2
+	.o_Add_result(Add_to_EXMEM_AddResult                               )
 );
 
 ALU i_ALU (
-	.i_Control   (i_Control   ),
-	.i_Data_1    (i_Data_1    ),
-	.i_Data_2    (i_Data_2    ),
-	.o_Zero      (o_Zero      ),
-	.o_ALU_Result(o_ALU_Result)
+	.i_Control   (ALUControl_to_ALU_Operation),
+	.i_Data_1    (IDEX_to_ALU_ReadData1      ),
+	.i_Data_2    (MuxALUSrc_to_ALU_Operand2  ),
+	.o_Zero      (ALU_to_EXMEM_Zero          ),
+	.o_ALU_Result(ALU_to_EXMEM_ALUResult     )
 );
 
-Mux i_Mux (
-	.i_Control(i_Chhhontrol), 
-	.i_Input_0(i_Input_0), 
-	.i_Input_1(i_Input_1), 
-	.o_Salida(o_Salida)
-	);
-	
+Mux i_Mux_ALUSrc (
+	.i_Control(ALUSrc                                          ),
+	.i_Input_0(IDEX_to_MuxALUSrc_to_EXMEM_ReadData2            ),
+	.i_Input_1(IDEX_to_ALUControl_to_Add_to_MuxALUSrc_Immediate),
+	.o_Salida (MuxALUSrc_to_ALU_Operand2                       )
+);
+
 ALU_control i_ALU_control (
-	.i_ALUOp(i_ALUOp), 
-	.i_Function_code(i_Function_code), 
-	.o_Operation(o_Operation)
-	);
-
-	
-Mux #(.BUS_SIZE(5)) i_Mux (
-	.i_Control(i_Control), 
-	.i_Input_0(i_Input_0), 
-	.i_Input_1(i_Input_1), 
-	.o_Salida(o_Salida));
+	.i_ALUOp        (ALUOp                                                ),
+	.i_Function_code(IDEX_to_ALUControl_to_Add_to_MuxALUSrc_Immediate[5:0]),
+	.o_Operation    (ALUControl_to_ALU_Operation                          )
+);
 
 
+Mux #(.BUS_SIZE(5)) i_Mux_RegDst (
+	.i_Control(RegDst                   ),
+	.i_Input_0(IDEX_to_MuxRegDst_rt_0   ),
+	.i_Input_1(IDEX_to_MuxRegDst_rd_1   ),
+	.o_Salida (MuxRegDst_to_EXMEM_Result)
+);
+
+EX_MEM i_EX_MEM (
+	.clk               (clk                                 ),
+	.rst               (rst                                 ),
+	.i_Add_result      (Add_to_EXMEM_AddResult              ),
+	.i_Zero            (ALU_to_EXMEM_Zero                   ),
+	.i_ALU_result      (ALU_to_EXMEM_ALUResult              ),
+	.i_Read_data_2     (IDEX_to_MuxALUSrc_to_EXMEM_ReadData2),
+	.i_MuxRegDst_result(MuxRegDst_to_EXMEM_Result           ),
+	.i_RegWrite        (EX_RegWrite                         ),
+	.i_MemtoReg        (EX_MemtoReg                         ),
+	.i_Branch          (EX_Branch                           ),
+	.i_MemRead         (EX_MemRead                          ),
+	.i_MemWrite        (EX_MemWrite                         ),
+	.o_Add_result      (o_Add_result                        ), // Completar
+	.o_Zero            (o_Zero                              ), // Completar
+	.o_ALU_result      (o_ALU_result                        ), // Completar
+	.o_Read_data_2     (o_Read_data_2                       ), // Completar
+	.o_MuxRegDst_result(o_MuxRegDst_result                  ), // Completar
+	.o_RegWrite        (o_RegWrite                          ), // Completar
+	.o_MemtoReg        (o_MemtoReg                          ), // Completar
+	.o_Branch          (o_Branch                            ), // Completar
+	.o_MemRead         (o_MemRead                           ), // Completar
+	.o_MemWrite        (o_MemWrite                          ) // Completar
+);
 
 
 
